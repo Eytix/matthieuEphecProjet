f33feb5c9395f06a4d898bdd706432e940c02a37
Romain Hardenne <romain.hardenne@metrix-consult.be>
2025-09-23 10:28:11 +0200

    Extract page structure into components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import {Component} from '@angular/core';
 import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
 import {MatToolbar} from '@angular/material/toolbar';
-import {MatListItem, MatNavList} from '@angular/material/list';
-import {MatIcon} from '@angular/material/icon';
-import {MatIconButton} from '@angular/material/button';
+import {MatNavList} from '@angular/material/list';
+import {FooterComponent} from './views/layout/footer/footer.component';
+import {HeaderComponent} from './views/layout/header/header.component';
+import {ContentComponent} from './views/layout/content/content.component';
+import {MenuComponent} from './views/layout/menu/menu.component';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +14,10 @@ import {MatIconButton} from '@angular/material/button';
     MatToolbar,
     MatNavList,
     MatSidenavContent,
-    MatIcon,
-    MatListItem,
-    MatIconButton],
+    FooterComponent, HeaderComponent, ContentComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular';
+
 }
